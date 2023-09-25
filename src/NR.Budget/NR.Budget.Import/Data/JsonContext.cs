e14ab1056822e88648fb9f57cd26ecdf8d339ff9
nrouelle <consulting@nadegerouelle.fr>
2023-09-25 15:07:53 +0200

    add a jsoncontext

---
+++ src/NR.Budget/NR.Budget.Import/Data/JsonContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace NR.Budget.Import.Data;
+
+public class JsonContext : IDataContext
+{
+    public List<BudgetLine> Lines { get; set; } = new List<BudgetLine>();
+    public void Save(List<BudgetLine> lines)
+    {
+        string path = "./accounts.json";
+        string json = JsonSerializer.Serialize(lines);
+        
+        File.WriteAllText(path, json);
+    }
+}
