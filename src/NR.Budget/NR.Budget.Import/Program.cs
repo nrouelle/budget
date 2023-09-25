d463d5383c4d9f4a92ffe50c4b0842c81101d50a
nrouelle <consulting@nadegerouelle.fr>
2023-09-25 15:08:03 +0200

    delete old code

--- src/NR.Budget/NR.Budget.Import/Program.cs
+++ src/NR.Budget/NR.Budget.Import/Program.cs
@@ -3,19 +3,3 @@
 using System.Xml.Serialization;
 
 var fileContent = File.ReadAllLines(args[0]);
-var datas = new List<BankingCsv>();
-foreach (var line in fileContent)
-{
-    var bkData = ImportBudgetLine(line);
-    datas.Add(bkData);
-    
-    BankingCsv ImportBudgetLine(string budgetLine)
-    {
-        var parsedLine = budgetLine.Split(';');
-        var bankingData = new BankingCsv(
-            DateTime.Parse(parsedLine[0]), 
-            parsedLine[2].Replace('"',' ').Trim(), 
-            decimal.Parse(parsedLine[5]));
-        return bankingData;
-    }
-}
