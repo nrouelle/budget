7573d51eda79542bf0b2f36b5e6b06a085cb9a44
nrouelle <consulting@nadegerouelle.fr>
2023-10-09 09:13:54 +0200

    wip: add react/blazor app

---
+++ src/NR.Budget/NR.Budget.Import.Test/GetDashBoardShould.cs
@@ -0,0 +1,20 @@
+﻿using NR.Budget.Import.Infrastructure;
+
+namespace NR.Budget.Import.Test;
+
+public class GetDashBoardShould
+{
+
+    [SetUp]
+    public void Init()
+    {
+    }
+    
+    [Test]
+    public void ReturnAllExpenseAndRevenue()
+    {
+        var context = new InMemoryContext();
+
+        // var dashboard = new DashBoardViewModel();
+    }
+}
